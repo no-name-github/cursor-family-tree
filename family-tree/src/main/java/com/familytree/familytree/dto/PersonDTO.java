407ed0a4c22099db36901951515b7bde31fd6819
no-name-github <no-name-git@nexjoin.com>
2025-04-27 16:15:01 -0700

    PersonService created

---
+++ family-tree/src/main/java/com/familytree/familytree/dto/PersonDTO.java
@@ -0,0 +1,26 @@
+package com.familytree.familytree.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class PersonDTO {
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String occupation;
+    private String lifeStory;
+    private LocalDate bornDate;
+    private LocalDate diedDate;
+    private String birthPlace;
+    private String currentlyLivesAtAddress;
+    
+    // Relationships
+    private Long motherId;
+    private Long fatherId;
+    private Long spouseId;
+    private Long parentId;
+} 
